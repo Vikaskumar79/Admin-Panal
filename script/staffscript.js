a7726a88e5ad4a0e1800d0267382f72f74737633
Vikas Nishad <vikas79058@gmail.com>
2025-07-14 03:06:36 +0530

    Add files via upload

---
+++ script/staffscript.js
@@ -0,0 +1,39 @@
+const form = document.querySelector("form"),
+        nextBtn = form.querySelector(".nextBtn"),
+        backBtn = form.querySelector(".backBtn"),
+        submitBtn = form.querySelector(".submitBtn"),
+        allInput = form.querySelectorAll(".first input");
+        const sameAddressCheckbox = document.getElementById("sameAddress");
+
+
+nextBtn.addEventListener("click", ()=> {
+    // allInput.forEach(input => {
+        // if(input.value != ""){
+            form.classList.add('secActive');
+        // }else{
+            // form.classList.remove('secActive');
+        // }
+    // })
+})
+
+backBtn.addEventListener("click", () => form.classList.remove('secActive'));
+
+
+
+sameAddressCheckbox.addEventListener("change", function () {
+    if (this.checked) {
+        document.getElementById("streetAddress").value = document.querySelector("input[placeholder='Street Address']").value;
+        document.getElementById("addressLine2").value = document.querySelector("input[placeholder='Street Address Line 2']").value;
+        document.getElementById("permanentState").value = document.querySelector("select[name='currstate']").value;
+        document.getElementById("district").value = document.querySelector("input[placeholder='District']").value;
+        document.getElementById("postOffice").value = document.querySelector("input[placeholder='Post Office']").value;
+        document.getElementById("perrZIP").value = document.querySelector("input[placeholder='Zip Code']").value;
+    } else {
+        document.getElementById("streetAddress").value = "";
+        document.getElementById("addressLine2").value = "";
+        document.getElementById("permanentState").value = "";
+        document.getElementById("district").value = "";
+        document.getElementById("postOffice").value = "";
+        document.getElementById("perrZIP").value = "";
+    }
+});
